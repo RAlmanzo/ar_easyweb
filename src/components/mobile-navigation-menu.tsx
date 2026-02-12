2f0e64b62d60b97ad7e06d3e0b16b01e6bb39aea
Almanzo Rosseel <ralmanzo@gmail.com>
2026-02-12 18:08:46 +0100

    Import component navigationlinks and add interface menuprops

--- src/components/mobile-navigation-menu.tsx
+++ src/components/mobile-navigation-menu.tsx
@@ -1,21 +1,17 @@
-import Link from "next/link";
+import NavigationLinks from "./navigation-links";
 
-interface MenuButtonProps {
+interface MenuProps {
     isOpen: boolean;
     setIsOpen: (isOpen: boolean) => void;
 }
 
-export default function MobileNavigationMenu({ isOpen, setIsOpen }: MenuButtonProps) {
+export default function MobileNavigationMenu({ isOpen, setIsOpen }: MenuProps) {
     return (
         <>
             {isOpen && (
                 <div className="md:hidden py-4 px-6 border-t border-border rounded-b-3xl">
                     <div className="flex flex-col gap-4 items-center">
-                        <Link href="/" onClick={() => setIsOpen(false)}>Home</Link>
-                        <Link href="/diensten" onClick={() => setIsOpen(false)}>Diensten</Link>
-                        <Link href="/portfolio" onClick={() => setIsOpen(false)}>Portfolio</Link>
-                        <Link href="/over" onClick={() => setIsOpen(false)}>Over Mij</Link>
-                        <Link href="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
+                        <NavigationLinks isOpen={isOpen} setIsOpen={setIsOpen} />
                     </div>
                 </div>
             )}
