c74590e98f9c328617e0c53793d150dd5ef39a5f
Almanzo Rosseel <ralmanzo@gmail.com>
2026-02-16 14:36:52 +0100

    Edit main tailwind css style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/sections/hero";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-[#eaf3e5]">
-      <main className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-32 px-16 bg-transparent sm:items-start">
+      <main className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-32 px-16 bg-transparent">
         <Hero />
       </main>
     </div>
