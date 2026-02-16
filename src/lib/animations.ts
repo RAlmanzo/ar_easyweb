d3ab3921ad33b3362f3288b14e0d859d642cf3a5
Almanzo Rosseel <ralmanzo@gmail.com>
2026-02-16 17:40:04 +0100

    Fix lint error in animations ts file

--- src/lib/animations.ts
+++ src/lib/animations.ts
@@ -2,5 +2,5 @@ export const fadeInUp = {
     initial: { opacity: 0, y: 60 },
     whileInView: { opacity: 1, y: 0 },
     viewport: { once: false, margin: "-100px" },
-    transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] as any },
+    transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] },
 }
