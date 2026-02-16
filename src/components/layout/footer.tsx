f1d2746290afb3e8712050f409db463b6a555480
Almanzo Rosseel <ralmanzo@gmail.com>
2026-02-16 22:29:22 +0100

    Set background color for footer component

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,10 +1,9 @@
 import { GitBranchPlus, Linkedin, Mail } from "lucide-react";
-import NavigationLinks from "./navigation-links";
 import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="bg-card border-t border-border">
+        <footer className="border-t border-border bg-[#F4F6F2]">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
                     <div className="space-y-4">
