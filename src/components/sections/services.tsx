dc5f9cb896f96fb823c136c04b89c7d7e797844f
Almanzo Rosseel <ralmanzo@gmail.com>
2026-02-16 17:57:04 +0100

    Import link from nextlink and button from shadcn ui components

--- src/components/sections/services.tsx
+++ src/components/sections/services.tsx
@@ -1,5 +1,7 @@
 import * as motion from "framer-motion/client"
 import { Card, CardContent } from "../ui/card";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 export default function Services() {
     return (
