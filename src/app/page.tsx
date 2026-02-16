4264c2cd78018482113e6a82c543043a7e493d07
Almanzo Rosseel <ralmanzo@gmail.com>
2026-02-16 16:47:54 +0100

    Import about component into homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import About from "@/components/sections/about";
 import Hero from "@/components/sections/hero";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#eaf3e5]">
-      <main className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-32 px-16 bg-transparent">
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-32 px-16 bg-[#eaf3e5]">
         <Hero />
+        <About />
       </main>
     </div>
   );
