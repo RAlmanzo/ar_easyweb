06cbf87ecfdbeb58c59269f45e520ff178865257
Almanzo Rosseel <ralmanzo@gmail.com>
2026-02-16 17:35:07 +0100

    Change tailwind css style for main element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Hero from "@/components/sections/hero";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <main className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-32 px-16 bg-[#eaf3e5]">
+    <div className="flex min-h-screen items-center justify-center bg-[#F4F6F2]">
+      <main className="flex min-h-screen w-full max-w-6xl flex-col items-center justify-between py-32 bg-[#eaf3e5]">
         <Hero />
         <About />
       </main>
