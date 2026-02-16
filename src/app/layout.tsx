f0662617b01aadd8fa21c819a9f2a8fc9de94493
Almanzo Rosseel <ralmanzo@gmail.com>
2026-02-16 22:24:04 +0100

    Import footer component into rootlayoutpage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navigation from "@/components/layout/navigation";
+import Footer from "@/components/layout/footer";
 
 
 const geistSans = Geist({
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Navigation />
         {children}
+        <Footer />
       </body>
     </html>
   );
