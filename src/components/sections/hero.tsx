ddd2f26a5a27a3839c56794687cfdaeb8fa2fa26
Almanzo Rosseel <ralmanzo@gmail.com>
2026-02-16 14:37:43 +0100

    Import logotext component into heropage and edit tailwind css styling

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,11 +1,12 @@
 import { ArrowRight } from "lucide-react";
 import Link from "next/link"
 import { Button } from "../ui/button";
+import LogoText from "../svg/easyweb-text-svg";
 
 export default function Hero() {
     return (
-        <section className="pt-3 pb-1 px-4 lg:px-8">
-            <div className="w-full max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8">
+        <section className="pt-2 pb-20 px-4 sm:px-6 lg:px-8 ">
+            <div className="w-full max-w-8xl mx-auto flex flex-col md:flex-row items-center justify-center pb-12">
                 <video
                     src="/logo.mp4"
                     autoPlay
@@ -14,17 +15,17 @@ export default function Hero() {
                     playsInline
                     className="w-72 md:w-96 h-auto rounded-lg"
                 />
-                <h1 className="font-extrabold text-4xl md:text-6xl">
-                    EasyWeb
+                <h1 className="font-extrabold text-4xl md:text-6xl text-foreground">
+                    <LogoText className="text-[#32CD32]" />
                 </h1>
             </div>
 
-            <div className="max-w-7xl mx-auto">
-                <h2 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-foreground mb-6 text-balance">
+            <div className="max-w-8xl mx-auto">
+                <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-foreground mb-6 text-balance">
                     Moderne websites voor KMO's
                 </h2>
                 <p className="text-lg sm:text-xl text-foreground mb-8 text-pretty leading-relaxed max-w-2xl mx-auto">
-                    Ik bouw snelle, veilige en schaalbare websites – van eenvoudige marketingwebsites tot headless
+                    Ik bouw snelle, veilige en schaalbare websites van eenvoudige marketingwebsites tot headless
                     CMS-oplossingen.
                 </p>
                 <div className="flex flex-col sm:flex-row gap-4 justify-center">
