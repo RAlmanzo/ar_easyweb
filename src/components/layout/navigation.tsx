cc0e84994f3477146993e2461e8cb333eecc9153
Almanzo Rosseel <ralmanzo@gmail.com>
2026-02-18 13:59:02 +0100

    Change background color for nav

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -11,7 +11,7 @@ export default function Navigation() {
 
     return (
         <nav className="fixed top-4 left-0 right-0 z-50 px-4">
-            <div className="max-w-4xl mx-auto bg-background/80 backdrop-blur-md border border-lime-700 rounded-full shadow-md">
+            <div className="max-w-4xl mx-auto bg-[linear-gradient(45deg,#999_5%,#fff_18%,#ccc_30%,#ddd_50%,#ccc_70%,#fff_80%,#999_95%)] backdrop-blur-md border border-lime-700 rounded-full shadow-md">
                 <div className="flex justify-between items-center h-14 px-6">
                     <Link href="/" className="text-xl font-bold text-foreground mx-auto md:mx-0">
                         AR EasyWeb
