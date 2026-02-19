b03f0e1a4644e01fd166140d2848f5671c26b50d
Almanzo Rosseel <ralmanzo@gmail.com>
2026-02-19 17:52:38 +0100

    Add const ctacontent in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,19 @@ import Hero from "@/components/sections/hero";
 import Services from "@/components/sections/services";
 
 export default function Home() {
+  const ctaContent = {
+    title: "Klaar voor een nieuwe website?",
+    description: "Laten we samen jouw project bespreken. Vrijblijvend en zonder verplichtingen.",
+    buttonText: "Vraag een offerte aan",
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center">
       <main className="flex min-h-screen w-full max-w-6xl flex-col items-center justify-between py-32 bg-foreground">
         <Hero />
         <About />
         <Services />
-        <Cta />
+        <Cta content={ctaContent} />
         <FAQ />
       </main>
     </div>
